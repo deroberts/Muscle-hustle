2a80f3ea238bc5e57a967e46a663255aabf1421c
deroberts <s501656@gmail.com>
2023-01-06 18:28:11 -0600

    made some changes to review routes in api and models, as well as user homeroutes, and handlebars to get this badboy more functional.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const axios = require('axios');
 const cheerio = require('cheerio');
 const carRoutes = require('./car-routes');
+const reviewRoutes = require('./reviewroutes');
 
+router.use('/reviews', reviewRoutes);
 router.use('/users', userroutes);
 router.use('/cars', carRoutes);
 
