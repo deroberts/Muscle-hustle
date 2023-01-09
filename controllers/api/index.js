75c322be61a9fb5e6c6b7f130021b3c575b802af
deroberts <s501656@gmail.com>
2023-01-08 18:46:24 -0600

    working on adding a reply field to reviews

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,13 +4,13 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const carRoutes = require('./car-routes');
 const reviewRoutes = require('./reviewroutes');
-const reviewCommentRoutes = require('./reviewcomment');
 
-router.use('/reviews', reviewRoutes);
+
+router.use('/review', reviewRoutes);
 router.use('/users', userroutes);
 router.use('/cars', carRoutes);
 //not sure if I need a separate route for review comments since they are nested in the review routes
-router.use('/reviewcomments', reviewCommentRoutes);
+
 
 
 
